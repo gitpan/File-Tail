57a6e785a9ac7bf3c00b05457dff3a919a4a565f
Matija Grabnar <matija+pause@serverflow.com>
2005-09-14 14:06:53 +0000

    Import of MGRABNAR/File-Tail-0.99.3 from CPAN.
    gitpan-cpan-distribution: File-Tail
    gitpan-cpan-version:      0.99.3
    gitpan-cpan-path:         MGRABNAR/File-Tail-0.99.3.tar.gz
    gitpan-cpan-author:       MGRABNAR
    gitpan-cpan-maturity:     released

--- Tail.pm
+++ Tail.pm
@@ -9,7 +9,7 @@ require Exporter;
 # Items to export into callers namespace by default. Note: do not export
 # names by default without a very good reason. Use EXPORT_OK instead.
 # Do not simply export all your public functions/methods/constants.
-$VERSION = '0.99.2';
+$VERSION = '0.99.3';
 
 
 # Preloaded methods go here.
